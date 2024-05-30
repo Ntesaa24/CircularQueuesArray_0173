ace0568e80f498b1a4b4a5891c62169c39698240
Ntesaa24 <notesa.aldinasari24@gmail.com>
2024-05-30 10:41:33 +0700

    kembali ke awal array

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -32,8 +32,11 @@ public:
 		else {
 			// Jika REAR berada di posisi terakhir array, kembali ke awal array 
 			if (REAR == max - 1)
-				REAR = 0; 
+				REAR = 0;
+			else
+				REAR = REAR + 1; 
 		}
+		queue_array[REAR] = num; 
 	}
 
 	
