c9033365b4d6b7b17d070ee1811956bb42439297
Ntesaa24 <notesa.aldinasari24@gmail.com>
2024-05-30 10:57:20 +0700

    FRONT +1

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -55,7 +55,9 @@ public:
 		else {
 			// jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array 
 			if (FRONT == max - 1)
-				FRONT = 0; 
+				FRONT = 0;
+			else
+				FRONT = FRONT + 1; 
 		}
 	}
 
