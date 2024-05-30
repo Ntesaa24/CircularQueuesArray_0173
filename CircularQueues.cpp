3dfcde14d0e5bebb3ac86259d4c484e44aae5769
Ntesaa24 <notesa.aldinasari24@gmail.com>
2024-05-30 10:56:33 +0700

    berada di posisi terakhir array

--- CircularQueues.cpp
+++ CircularQueues.cpp
@@ -52,6 +52,11 @@ public:
 			FRONT = -1; 
 			REAR = -1; 
 		}
+		else {
+			// jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array 
+			if (FRONT == max - 1)
+				FRONT = 0; 
+		}
 	}
 
 	
